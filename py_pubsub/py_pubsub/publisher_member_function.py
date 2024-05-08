8b5a98672a76c36d7387befb06469968be0f5bcf
Piatan Sfair Palar <piatan@alunos.utfpr.edu.br>
2024-05-08 13:38:01 -0300

    modified:   py_pubsub/py_pubsub/publisher_member_function.py

--- py_pubsub/py_pubsub/publisher_member_function.py
+++ py_pubsub/py_pubsub/publisher_member_function.py
@@ -15,25 +15,51 @@
 import rclpy
 from rclpy.node import Node
 
-from std_msgs.msg import String
+from sensor_msgs.msg import JointState
 
 
 class MinimalPublisher(Node):
 
     def __init__(self):
         super().__init__('minimal_publisher')
-        self.publisher_ = self.create_publisher(String, 'topic', 10)
+        self.publisher_ = self.create_publisher(JointState, 'joint_states', 10)
         timer_period = 0.5  # seconds
         self.timer = self.create_timer(timer_period, self.timer_callback)
         self.i = 0
+        self.subscription = self.create_subscription(
+            JointState,
+            'joint_states',
+            self.listener_callback,
+            10)
+        self.subscription  # prevent unused variable warning
+
+    def listener_callback(self, msg):
+        global mensagem
+        mensagem = msg
+        self.get_logger().info('I heard: "%s"' % msg.position)
 
     def timer_callback(self):
-        msg = String()
-        msg.data = 'Hello World: %d' % self.i
+        global mensagem
+        msg = mensagem
+        msg = JointState()
+        msg.name.append('leftjoint_1')
+        msg.name.append('leftjoint_2')
+        msg.name.append('leftjoint_3')
+        msg.name.append('leftjoint_4')
+        msg.name.append('leftjoint_5')
+        msg.name.append('leftjoint_6')
+        msg.name.append('rightjoint_1')
+        msg.name.append('rightjoint_2')
+        msg.name.append('rightjoint_3')
+        msg.name.append('rightjoint_4')
+        msg.name.append('rightjoint_5')
+        msg.name.append('rightjoint_6')
+        msg.position.append(47)
+        msg.position.append(48)
         self.publisher_.publish(msg)
-        self.get_logger().info('Publishing: "%s"' % msg.data)
         self.i += 1
 
+mensagem = JointState()
 
 def main(args=None):
     rclpy.init(args=args)
