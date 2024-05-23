19a98ffb4ac03eabf1733536963ea7090a0fad93
Piatan Sfair Palar <piatan@alunos.utfpr.edu.br>
2024-05-23 13:05:59 -0300

    modified:   Comandos.txt 	new file:   tm_moveit_cpp_demo/src/moveit_cpp_tutorial.cpp 	modified:   tm_moveit_cpp_demo/src/run_moveit_cpp.cpp

--- tm_moveit_cpp_demo/src/run_moveit_cpp.cpp
+++ tm_moveit_cpp_demo/src/run_moveit_cpp.cpp
@@ -64,14 +64,11 @@ public:
   void run()
   {
     RCLCPP_INFO(LOGGER, "Initialize MoveItCpp");
-    //moveit_cpp_ = std::make_shared<moveit::planning_interface::MoveItCpp>(node_);
     moveit_cpp_ = std::make_shared<moveit_cpp::MoveItCpp>(node_);
     moveit_cpp_->getPlanningSceneMonitor()->providePlanningSceneService();  // let RViz display query PlanningScene
     moveit_cpp_->getPlanningSceneMonitor()->setPlanningScenePublishingFrequency(100);
 
     RCLCPP_INFO(LOGGER, "Initialize PlanningComponent");
-    //moveit::planning_interface::PlanningComponent arm_left("left_tmr_arm", moveit_cpp_);
-    //moveit::planning_interface::PlanningComponent arm_right("right_tmr_arm", moveit_cpp_);
     moveit_cpp::PlanningComponent arm_left("left_tmr_arm", moveit_cpp_);
     moveit_cpp::PlanningComponent arm_right("right_tmr_arm", moveit_cpp_);
 
@@ -91,10 +88,10 @@ public:
     //arm_right.setGoal("rightready3");
     
     //arm_left.setGoal("lefthome");   
-    arm_right.setGoal("rightready1");  
+    arm_right.setGoal("righthome");  
 
     // Define a sequência de goals para o braço esquerdo
-    std::vector<std::string> left_arm_goals = {"leftready1","lefthome"};
+    std::vector<std::string> left_arm_goals = {"lefthome", "leftready1"};
 
     // Itera sobre os goals do braço esquerdo
     for (const auto& goal : left_arm_goals)
@@ -135,27 +132,11 @@ public:
       arm_right.execute();
     }
 
-    //Below, we simply use a long delay to wait for the previous motion to complete.
-    /*rclcpp::sleep_for(std::chrono::seconds(10));   
-
-    // Set joint state goal
-    RCLCPP_INFO(LOGGER, "Set goal (home)");
-    arm.setGoal("home");-
-
-    // Run actual plan
-    RCLCPP_INFO(LOGGER, "Plan to goal");
-    plan_solution = arm.plan();
-    if (plan_solution)
-    {
-      RCLCPP_INFO(LOGGER, "arm.execute()");
-      arm.execute();
-    }*/
   }
 
 private:
   rclcpp::Node::SharedPtr node_;
   rclcpp::Publisher<moveit_msgs::msg::DisplayRobotState>::SharedPtr robot_state_publisher_;
-  //moveit::planning_interface::MoveItCppPtr moveit_cpp_;
   moveit_cpp::MoveItCppPtr moveit_cpp_;
 };
 
